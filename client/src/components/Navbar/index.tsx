c27a212cd5941d984d84c356f1268769d80f72cf
Luiz Ferreira <lh.alvesf@gmail.com>
2026-05-21 19:09:12 -0300

    navbar component

---
+++ client/src/components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+export function Navbar() {
+    return(<>
+        <nav>
+            <ul>
+                <li><a href="">Pointing poker</a></li>
+                <li><a href="">1</a></li>
+                <li><a href="">2</a></li>
+                <li><a href="">3</a></li>
+            </ul>
+        </nav>
+    </>)
+}
